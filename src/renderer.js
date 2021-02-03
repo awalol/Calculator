693586e3f86b6a6db494089a671248d1a4db1435
CNawalol <awalol@aliyun.com>
2021-02-03 23:30:31 +0800

    fix some bug

--- src/renderer.js
+++ src/renderer.js
@@ -11,10 +11,10 @@
 // })
 
 document.getElementById('r0-result').addEventListener('click', async() => {
-    var L = document.getElementById('r0-L').value
-    var dt = document.getElementById('r0-dt').value
-    var d3 = document.getElementById('r0-d3').value
-    var a3 = document.getElementById('r0-a3').value
+    var L = parseFloat(document.getElementById('r0-L').value)
+    var dt = parseFloat(document.getElementById('r0-dt').value)
+    var d3 = parseFloat(document.getElementById('r0-d3').value)
+    var a3 = parseFloat(document.getElementById('r0-a3').value)
 
     var result = (Math.pow(L,2) + Math.pow((1.5 - d3 / (2 * dt)),2) - 1) / (2 * (1 - L * Math.sin(a3 * (2 * Math.PI / 360)) - (1.5 - d3 / (2 * dt)) * Math.cos(a3 * (2 * Math.PI / 360))))
 
@@ -22,17 +22,17 @@ document.getElementById('r0-result').addEventListener('click', async() => {
 })
 
 document.getElementById('x0-result').addEventListener('click', async() => {
-    var L = document.getElementById('x0-L').value
-    var R0 = document.getElementById('x0-r0').value
-    var a3 = document.getElementById('x0-a3').value
+    var L = parseFloat(document.getElementById('x0-L').value)
+    var R0 = parseFloat(document.getElementById('x0-r0').value)
+    var a3 = parseFloat(document.getElementById('x0-a3').value)
 
     document.getElementById('dialog-content').innerHTML = L + R0 * Math.sin(a3 * (2 * Math.PI / 360))
 })
 
 document.getElementById('y0-result').addEventListener('click', async() => {
-    var R0 = document.getElementById('y0-r0').value
-    var a3 = document.getElementById('y0-d3').value
-    var d3 = document.getElementById('y0-d3').value
+    var R0 = parseFloat(document.getElementById('y0-r0').value)
+    var a3 = parseFloat(document.getElementById('y0-d3').value)
+    var d3 = parseFloat(document.getElementById('y0-d3').value)
 
     document.getElementById('dialog-content').innerHTML = R0 * Math.cos(a3 * (2 * Math.PI / 360)) - d3 / 2
 })
